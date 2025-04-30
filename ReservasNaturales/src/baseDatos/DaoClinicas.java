0b2d6b6b9285a4c6399abca1a0359a2c46227dc4
Miguel Trigo Martin de Vidales <migueltrigomartindevidales@gmail.com>
2025-04-30 11:08:19 +0200

    Add files via upload

---
+++ ReservasNaturales/src/baseDatos/DaoClinicas.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package baseDatos;
+
+import aplicacion.FachadaAplicacion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import aplicacion.ClinicaMedica;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author alumnogreibd
+ */
+
+public class DaoClinicas extends AbstractDAO{
+
+    public DaoClinicas(Connection conexion, FachadaAplicacion fa) {
+        super.setConexion(conexion);
+        super.setFachadaAplicacion(fa);
+    }
+    public List<ClinicaMedica> obtenerClinicas(String textoBusqueda) {
+    List<ClinicaMedica> resultado = new ArrayList<>();
+    Connection con = this.getConexion();
+    PreparedStatement stmClinica = null;
+    ResultSet rsClinica;
+
+    try {
+        String consulta = "SELECT nombre, ubicacion, numempleados FROM clinicas WHERE nombre ILIKE ?";
+        stmClinica = con.prepareStatement(consulta);
+        stmClinica.setString(1, "%" + textoBusqueda + "%");
+        rsClinica = stmClinica.executeQuery();
+
+        while (rsClinica.next()) {
+            String nombre = rsClinica.getString("nombre");
+            String ubicacion = rsClinica.getString("ubicacion");
+            int numEmpleados = rsClinica.getInt("numempleados");
+
+            ClinicaMedica c = new ClinicaMedica(nombre, ubicacion, numEmpleados);
+            resultado.add(c);
+        }
+
+    } catch (SQLException e) {
+        System.out.println(e.getMessage());
+        this.getFachadaAplicacion().muestraExcepcion(e.getMessage());
+    } finally {
+        try {
+            if (stmClinica != null) stmClinica.close();
+        } catch (SQLException e) {
+            System.out.println("Imposible cerrar cursores");
+        }
+    }
+
+    return resultado;
+}
+
+    
+}
